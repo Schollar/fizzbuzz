ba3af442b8dd891a927a29137039cd7d3381dc41
schollar <cschollar22@gmail.com>
2022-01-22 14:33:32 -0700

    Got working version of fizzbuzz function

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 def fizzbuzz(num):
-    if(num % 3 == 0):
+    if(num % 3 == 0 and num % 5 == 0):
+        print('FizzBuzz')
+    elif(num % 3 == 0):
         print("Fizz")
     elif(num % 5 == 0):
         print('Buzz')
-    elif(num % 3 == 0 and num % 5 == 0):
-        print('FizzBuzz')
     else:
         print(num)
 
