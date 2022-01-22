e1f545a5f9cbd202a8b79c41a4bdd7d94e53e67d
schollar <cschollar22@gmail.com>
2022-01-22 14:28:37 -0700

    Started Fizzbuzz assignment

---
+++ app.py
@@ -0,0 +1,15 @@
+def fizzbuzz(num):
+    if(num % 3 == 0):
+        print("Fizz")
+    elif(num % 5 == 0):
+        print('Buzz')
+    elif(num % 3 == 0 and num % 5 == 0):
+        print('FizzBuzz')
+    else:
+        print(num)
+
+
+numbers = [2, 3, 6, 19, 25, 15, 50, 33, 27, 99, 85, 100, 105, 115]
+
+for number in numbers:
+    fizzbuzz(number)
